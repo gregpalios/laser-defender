543319532e7708c8c63d238760e0926888aeef16
Greg Palios <56395844+gregpalios@users.noreply.github.com>
2020-08-20 11:37:52 -0400

    Add files via upload

---
+++ Entities/Enemy/EnemyFormation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFormation : MonoBehaviour {
+	public GameObject projectile;
+	public float projectileSpeed = 10f;
+	public float health = 150f;
+	public float shotsPerSeconds = 0.5f;
+	public int scoreValue = 150;
+	
+	public AudioClip fireSound;
+	public AudioClip deathSound;
+	
+	private ScoreKeeper scoreKeeper;
+	
+	void Start(){
+		scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+	}
+	
+	void Update(){
+		float probability = Time.deltaTime * shotsPerSeconds;
+		if(Random.value < probability){
+			Fire ();
+		}
+	}
+
+	void Fire(){
+		GameObject missile = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		missile.rigidbody2D.velocity = new Vector2(0,-projectileSpeed);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Die ();
+			}
+		}
+	}
+	void Die(){
+		AudioSource.PlayClipAtPoint(deathSound, transform.position);
+		Destroy(gameObject);
+		scoreKeeper.Score(scoreValue);
+	}
+}
---
+++ Entities/Player/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 15.0f;
+	public float padding = 1f;
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
+	public float health = 250f;
+	
+	public AudioClip fireSound;
+	
+	float xmin;
+	float xmax;
+	
+	void Start(){
+		// work out the boundaries of the playspace
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+	}
+	
+	void Fire(){
+		Vector3 offset = new Vector3(0, 1, 0);
+		GameObject beam = Instantiate(projectile, transform.position+offset, Quaternion.identity) as GameObject;
+		beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed, 0);
+		AudioSource.PlayClipAtPoint(fireSound, transform.position);
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			InvokeRepeating("Fire", 0.000001f, firingRate);
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			CancelInvoke("Fire");
+		}
+	
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+		// restrict the player to the playspace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log ("Player Collided with missile");
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Die();
+			}
+		}
+	}
+		
+	void Die(){
+		LevelManager man = GameObject.Find ("LevelManager").GetComponent<LevelManager>();
+		man.LoadLevel("Win Screen");
+		Destroy(gameObject);
+	}
+}
